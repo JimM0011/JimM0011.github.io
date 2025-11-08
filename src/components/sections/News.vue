eb777295e61be9f19e4d2b2f93564a5a0d70bbf5
JimM0011 <jingma0322@163.com>
2025-11-09 00:52:53 +0800

    update

--- src/components/sections/News.vue
+++ src/components/sections/News.vue
@@ -14,10 +14,19 @@
         <div class="news-context">
             <div class="news-item">
                 <div class="news-item-icon"></div>
+                <div class="news-item-date">11 / 2025</div>
+                <div class="news-item-content">
+                    <p>
+                        🎊 The <a href="https://arxiv.org/abs/2511.03256" target="_blank">paper</a> and <a href="https://github.com/HAIV-Lab/DEM" target="_blank">code</a> of <a href="https://jimm0011.github.io/DEM-proj/" target="_blank">DEM</a> ( <a href="https://neurips.cc/Conferences/2025" target="_blank">NeurIPS 2025</a> ) are publicly available. Please refer to the <a href="https://jimm0011.github.io/DEM-proj/" target="_blank">project page</a> for details.
+                    </p>
+                </div>
+            </div>
+            <div class="news-item">
+                <div class="news-item-icon"></div>
                 <div class="news-item-date">09 / 2025</div>
                 <div class="news-item-content">
                     <p>
-                        🎉 One paper ( <a href="https://neurips.cc/virtual/2025/poster/119274" target="_blank">DEM</a> ) has been accepted by <a href="https://neurips.cc/Conferences/2025" target="_blank">NeurIPS 2025</a>. The paper and code are coming soon.
+                        🎉 One paper ( <a href="https://jimm0011.github.io/DEM-proj/" target="_blank">DEM</a> ) has been accepted by <a href="https://neurips.cc/Conferences/2025" target="_blank">NeurIPS 2025</a>.
                     </p>
                 </div>
             </div>
@@ -35,7 +44,7 @@
                 <div class="news-item-date">07 / 2025</div>
                 <div class="news-item-content">
                     <p>
-                        🎊 The <a href="https://openreview.net/pdf?id=SznX4yic20" target="_blank">paper</a> and <a href="https://github.com/HAIV-Lab/PTTA" target="_blank">code</a> for <a href="https://jimm0011.github.io/PTTA-proj/" target="_blank">PTTA</a> ( <a href="https://icml.cc/Conferences/2025" target="_blank">ICML 2025</a> ) are publicly available. Please refer to the <a href="https://icml.cc/virtual/2025/poster/45190" target="_blank">project page</a> for details.
+                        🎊 The <a href="https://openreview.net/pdf?id=SznX4yic20" target="_blank">paper</a> and <a href="https://github.com/HAIV-Lab/PTTA" target="_blank">code</a> of <a href="https://jimm0011.github.io/PTTA-proj/" target="_blank">PTTA</a> ( <a href="https://icml.cc/Conferences/2025" target="_blank">ICML 2025</a> ) are publicly available. Please refer to the <a href="https://icml.cc/virtual/2025/poster/45190" target="_blank">project page</a> for details.
                     </p>
                 </div>
             </div>
